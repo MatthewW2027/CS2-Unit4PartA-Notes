bcf503cc002b9187f906738b530ae58c25743fa5
Matthew <149590049+MatthewW2027@users.noreply.github.com>
2026-01-29 19:26:13 +0000

    4.3 Notes

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
-   }
-}
+      // ID ARRAYS is a block of memory that stores a COLLECTION of data items (elements) of the same type UNDER ONE VARIABLE NAME!!! 
+
+      // How to DECLARE & CREATE an empty Array variable 
+      String[] apCompSci = new String[13]; 
+      double[] prices = new double[5]; // default values: 0.0 
+      String[] names = new String[5]; // default value: null 
+      int[] luckyNums = new int[10]; // default value: 0 
+      boolean[] yesNo = new boolean[3]; // default value: 
+
+      // How to ACCESS an item in an Array 
+      System.out.println(prices[0]); 
+
+   } // end main METHOD 
+} // END Main CLASS 
